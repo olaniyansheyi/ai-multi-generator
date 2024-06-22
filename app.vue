6dfaa6d49b23e349e6d6081b2602f26deed66cf6
sheymantech <olaniyansheyi1704@gmail.com>
2024-06-22 16:59:31 +0100

    new feature

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Button> All will be well! </Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  components: [
+    {
+      path: "~/components/ui",
+      extensions: [".vue"],
+      prefix: "",
+    },
+    {
+      path: "~/components",
+      extensions: [".vue"],
+      prefix: "",
+    },
+  ],
+});
